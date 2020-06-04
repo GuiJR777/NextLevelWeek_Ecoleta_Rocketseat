356c70fddd6872ec570ef715f7ca7a6bb382a984
Guilherme J. Ramires <guilhermejramires@gmail.com>
2020-06-04 19:32:23 -0300

    'Aula_04_Add_estrutura_de_Pastas_+_NodeJS_+_Servidor_rodando_em_backend'

---
+++ src/server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const server = express()
+
+server.use(express.static("public"))
+
+const nunjucks= require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+//configurar o caminho
+
+server.get("/", (req, res) => {
+   return res.render("index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search-results", (req, res) => {
+    return res.render("search-results.html")
+})
+
+
+server.listen(3000)
